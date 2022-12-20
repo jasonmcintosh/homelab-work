59d5f413d9de65c0dc48e37890caa77fa1e0b974
Jason McIntosh <jason.mcintosh@armory.io>
2022-12-20 12:45:39 -0600

    Updated demo web app to trigger pipeline

--- applications/demo-web-app/src/test/java/io/armory/canary/demo/demowebapp/DemoWebAppApplicationTests.java
+++ applications/demo-web-app/src/test/java/io/armory/canary/demo/demowebapp/DemoWebAppApplicationTests.java
@@ -1,13 +1,31 @@
 package io.armory.canary.demo.demowebapp;
 
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.web.servlet.MockMvc;
+
+import static org.hamcrest.Matchers.containsString;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
 @SpringBootTest
+@AutoConfigureMockMvc
 class DemoWebAppApplicationTests {
 
+	@Autowired
+	private MockMvc mockMvc;
+
 	@Test
-	void contextLoads() {
+	void makeSureUrlIsIncluded() throws Exception {
+		this.mockMvc.perform(get("/"))
+				.andDo(print())
+				.andExpect(status().isOk())
+				.andExpect(content().contentType("application/json"))
+				.andExpect(content().string(containsString("https://www.schlockmercenary.com/")));
 	}
 
 }
