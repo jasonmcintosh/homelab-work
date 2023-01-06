c0bed78943e612a8edfd391be86f2d32dafad14d
Jason McIntosh <jason.mcintosh@armory.io>
2023-01-06 02:00:01 -0600

    Fiddling with tf automation for encryption

---
+++ applications/tf-dns-example/main.tf
@@ -0,0 +1,56 @@
+terraform {
+  required_providers {
+    acme = {
+      source = "vancluever/acme"
+      version = "2.12.0"
+    }
+  }
+  backend "kubernetes" {
+    secret_suffix    = "tf-dns-encrypt"
+    in_cluster_config = true
+  }
+}
+provider "acme" {
+  server_url = "https://acme-staging-v02.api.letsencrypt.org/directory"
+}
+
+resource "tls_private_key" "private_key" {
+  algorithm = "RSA"
+}
+resource "acme_registration" "reg" {
+  account_key_pem = tls_private_key.private_key.private_key_pem
+  email_address   = "mcintoshj@gmail.com"
+}
+
+data "kubernetes_secret" "cloudflare-api" {
+  metadata {
+    name = "cloudflare-api-key"
+  }
+}
+
+resource "acme_certificate" "certificate" {
+  account_key_pem           = acme_registration.reg.account_key_pem
+  common_name               = "*.mcintosh.farm"
+  dns_challenge {
+    provider = "cloudflare"
+    config = {
+      CF_API_EMAIL=data.kubernetes_secret.cloudflare-api.data["api_email"]
+      CF_API_KEY=data.kubernetes_secret.cloudflare-api.data["api_key"]
+    }
+  }
+}
+
+resource "kubernetes_secret" "wildcard_cert" {
+  metadata {
+    name = "mcintosh-farm-certs"
+  }
+
+  binary_data = {
+    private_key_pem = base64encode(acme_registration.reg.account_key_pem)
+    issuer_pem = base64encode(acme_certificate.certificate.issuer_pem)
+    certificate_pem = base64encode(acme_certificate.certificate.certificate_pem)
+    combined_pem = base64encode("${acme_certificate.certificate.certificate_pem}${acme_certificate.certificate.issuer_pem}")
+    cert_p12 = base64encode(acme_certificate.certificate.certificate_p12)
+  }
+
+}
