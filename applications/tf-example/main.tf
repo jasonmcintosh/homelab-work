f2adc0b2f220fc40212cce7b5d204d5d371427f9
Jason McIntosh <jason.mcintosh@armory.io>
2023-01-06 12:46:00 -0600

    superSimpleTFExample

---
+++ applications/tf-example/main.tf
@@ -0,0 +1,20 @@
+terraform {
+
+  backend "kubernetes" {
+    secret_suffix    = "tf-timestamp-example"
+    in_cluster_config = true
+  }
+}
+}
+
+## Generating a SUPER simple example of updating a configmap with the current timestmap for demoing TF pipelines
+resource "kubernetes_configmap" "sample" {
+  metadata {
+    name = "last-run-timestamp"
+    namespace = "default"
+  }
+
+  data = {
+    last_run_timestamp = timestamp()
+  }
+}
